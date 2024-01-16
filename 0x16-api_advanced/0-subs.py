c2a5d2921743be86448c7f2fcb9f5011e52339f2
mariammagedfa <mariammagedfa@yahoo.com>
2024-01-16 15:18:48 -0800

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""my first Reddit API request"""
+"""  this function quersy subscribers on a Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    """the function return the number of subcribers"""
-    url = 'https://www.reddit.com/r/{}/.json'.format(subreddit)
-    header = {'User-Agent': 'Reddit API test'}
 
-    response = requests.get(url, headers=header, allow_redirects=False)
-    dict = response.json()
-    if dict.get("error", 200) == 404:
+def number_of_subscribers(subreddit):
+    """  this returns the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    return dict.get("data").get("children")[0].get("data")\
-        .get("subreddit_subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers")
